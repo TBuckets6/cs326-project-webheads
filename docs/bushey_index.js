4f5b0468fe5ff51525476af2027216e057c6382d
Taylor Bushey <tbushey236@gmail.com>
2022-08-06 19:54:26 -0400

    adding settings to UI

--- docs/bushey_index.js
+++ docs/bushey_index.js
@@ -1,8 +1,41 @@
 const codingPic = document.getElementById('codingPic');
+const mathPic = document.getElementById('mathPic');
+const bballPic = document.getElementById('bballPic')
 const topics = document.getElementById('topics');
+const difficulty = document.getElementById('difficulty');
+const timeLimit = document.getElementById('timeLimit');
+const startBtn = document.getElementById('start');
 
 codingPic.addEventListener("click", () => {
-    topics.style.display = "none";
+    difficulty.removeAttribute("disabled");
+    timeLimit.removeAttribute("disabled");
+    startBtn.removeAttribute("disabled");
+});
+
+mathPic.addEventListener("click", () => {
+    difficulty.removeAttribute("disabled");
+    timeLimit.removeAttribute("disabled");
+    startBtn.removeAttribute("disabled");
     
-})
+    
+    /*
+    topics.style.display = "none";
+    document.getElementById('header2').style.display = "none";
+    const difficulty = document.createElement("div"); 
+    const normal_diff = document.createElement("input");
+    normal_diff.setAttribute('type', 'button');
+    normal_diff.setAttribute('value', 'Normal');
+    normal_diff.setAttribute("id", "normal");
+    difficulty.appendChild(normal_diff);
+    document.body.appendChild(difficulty);
+    */
+});
+
+bballPic.addEventListener("click", () => {
+    difficulty.removeAttribute("disabled");
+    timeLimit.removeAttribute("disabled");
+    startBtn.removeAttribute("disabled");
+});
+
+
 
