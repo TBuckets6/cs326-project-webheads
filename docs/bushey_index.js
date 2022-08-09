e8dbf391a9a898f38810f6f078000f67e948bcb6
Taylor Bushey <tbushey236@gmail.com>
2022-08-08 22:33:10 -0400

    all questions work

--- docs/bushey_index.js
+++ docs/bushey_index.js
@@ -1,4 +1,4 @@
-import { hardLevelCoding, normalLevelCoding } from "./bushey_questions_answers.js";
+import { hardLevelCoding, normalLevelCoding, normalLevelMath, hardLevelMath, normalLevelBball, hardLevelBball } from "./bushey_questions_answers.js";
 
 const codingPic = document.getElementById('codingPic');
 const mathPic = document.getElementById('mathPic');
@@ -9,95 +9,76 @@ const timeLimit = document.getElementById('timeLimit');
 const startBtn = document.getElementById('start');
 const initDisplay = document.getElementById('initDisplay');
 const submitBtn = document.createElement('input');
-
-function saveState(){
-    window.localStorage.setItem('difficulty', JSON.stringify(difficulty.value)); //saves the difficulty as a string ("normal" or "hard").
-}
-
-function restoreState(){
-    let diff = window.localStorage.getItem('difficulty');  
-}
-
-function clearState(){
-    window.localStorage.clear();
-}
-
-function addPre(){
-    return document.createElement('pre');
-}
-
+//const quizDisplay = document.createElement('')
 /*
-function getAnswers(quiz){
-    let answers = []
-    for(let i = 0; i < 3; i++){
-        answers.push(quiz[i].correct)
-    }
-    return answers;
+if(window.localStorage){
+    restoreState();
 }
 */
 
-//const ans = getAnswers(quiz);
+function saveState() {
+    window.localStorage.setItem('difficulty', JSON.stringify(difficulty.value)); //saves the difficulty as a string ("normal" or "hard").
+    window.localStorage.setItem('quizDisplay', JSON.stringify(document.getElementById('quizDisplay')))
+}
 
-function displayQuiz(quiz){
+function restoreState() {
+    let diff = window.localStorage.getItem('difficulty');
+    initDisplay.style.display = "none";
+    document.body.appendChild(window.localStorage.getItem('quizDisplay'));
+
+}
+
+function clearState() {
+    window.localStorage.clear();
+}
+
+function addPre() {
+    return document.createElement('pre');
+}
+
+function displayQuiz(quiz) {
     let answers = [];
     let quizDisplay = document.createElement('div');
     let correctAnswer = 0;
     quizDisplay.setAttribute('id', 'quizDisplay');
-    //for(let i = 0; i < )
 
-    
-    for(const obj in quiz){
-   
-    let index0 = Number(obj) + 1;
-    let y = 'question' + index0
-    let q = document.createTextNode(quiz[obj][y]);
-    quizDisplay.appendChild(addPre())
-    quizDisplay.appendChild(q)
-    answers.push(quiz[obj].correct)
+    for (const obj in quiz) {
 
-    for(let i = 0; i < 4; i++){
-        /*
-        if(quiz[obj.correct === true]){
-            correctAnswer = obj;
-            answers.push(correctAnswer);
+        let index0 = Number(obj) + 1;
+        let y = 'question' + index0
+        let q = document.createTextNode(quiz[obj][y]);
+        quizDisplay.appendChild(addPre())
+        quizDisplay.appendChild(q)
+        answers.push(quiz[obj].correct)
+
+        for (let i = 0; i < 4; i++) {
+
+            let index = Number(obj) + 1
+            let x = 'answers' + index;
+            let choice = document.createElement('input');
+            let choiceTxt = document.createElement('label');
+            choice.setAttribute('type', 'radio');
+
+            choice.setAttribute('id', 'choice' + String(obj) + String(i));
+            choice.setAttribute('name', 'choice' + String(obj))
+            choiceTxt.textContent = quiz[obj][x][i][i];
+            choiceTxt.setAttribute('for', choice);
+            quizDisplay.appendChild(addPre());
+            quizDisplay.appendChild(choice)
+            quizDisplay.appendChild(choiceTxt)
+
         }
-        */
-        
-        let index = Number(obj) + 1
-        let x = 'answers' + index;
-        let choice = document.createElement('input');
-        let choiceTxt = document.createElement('label');
-        choice.setAttribute('type', 'radio');
-         
-        choice.setAttribute('id', 'choice' + String(obj) + String(i));
-        //console.log('choice' + String(obj) + String(i))
-        choice.setAttribute('name', 'choice' + String(obj))
-        //choiceTxt.textContent = quiz[0].answers1[0][property[0]];
-        choiceTxt.textContent = quiz[obj][x][i][i];
-        //console.log(choice)
-        choiceTxt.setAttribute('for', choice);
-        quizDisplay.appendChild(addPre());
-        quizDisplay.appendChild(choice)
-        quizDisplay.appendChild(choiceTxt)
-        
-    }
 
     }
     quizDisplay.appendChild(addPre())
-    //const finishBtn = document.createElement('input');
     submitBtn.setAttribute('type', 'button');
     submitBtn.setAttribute('value', 'Submit')
     quizDisplay.appendChild(submitBtn)
     document.body.appendChild(quizDisplay);
-    //return quiz;
-    //console.log(answers)
     return answers;
 }
-//console.log(quizDisplay)
-//const ans = quizDisplay()
 
-
-submitBtn.addEventListener("click", () =>{
+submitBtn.addEventListener("click", () => {
     document.getElementById('quizDisplay').style.display = "none";
     calculateScore();
     let resultsDisplay = document.createElement('div');
@@ -108,135 +89,57 @@ submitBtn.addEventListener("click", () =>{
 
 
 
-function calculateScore(){//this function will return the score
+function calculateScore() {//this function will return the score
     let answers = []
-    if(startBtn.classList.contains("codingPic")){
-        if(difficulty.value === "normal"){
-            answers = [0,2,1]
+    if (startBtn.classList.contains("codingPic")) {
+        if (difficulty.value === "normal") {
+            answers = [0, 2, 1]
 
 
         }
-        else{
-            displayQuiz(hardLevelCoding);
+        else {
+            answers = [2, 1, 1]
         }
 
     }
-    let checked = []
-    let zip = '';
+    else if(startBtn.classList.contains("mathPic")){
+        if (difficulty.value === "normal") {
+            answers = [1, 2, 3]
+
+
+        }
+        else {
+            answers = [3, 2, 2]
+        }
+    }
+    else{
+        if (difficulty.value === "normal") {
+            answers = [0, 2, 0]
+
+
+        }
+        else {
+            answers = [2, 2, 2]
+        }
+    }
+
+
     let score = 0;
-    for(let i = 0; i < 3; i++){
-        for(let j = 0; j < 4; j++){
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 4; j++) {
             let index = Number(i) + 1
-            let x = 'answers' + index;
-            if(document.getElementById('choice' + String(i) + String(j)).checked === true && j === answers[i]/*&&  j === */)  {
+            if (document.getElementById('choice' + String(i) + String(j)).checked === true && j === answers[i]) {
                 score = score + 1;
             }
-            
-            //if(document.getElementById('choice' + String(i) + String(j)).checked === true && q[i.correct] === /*q[i][x][j][j]*/){
-              //  console.log('hi')
-            //} 
         }
     }
-   
-    /*
-    for(let h = 0; h < 3; h++){
-        for(let i = 0; i < 4; i ++){
-            //console.log(typeof(choice01))
-            //let zip = JSON.parse('choice' + String(h) + String(i))
-            //console.log(JSON.parse(zip))
-            //if()
-            //console.log('hi')
-            //let zz ==  
-            //q[h][answers + h + 1][i][i + 1] //to get the 'correct' property?
-            //if(q[h]){
-
-            //}
-            let index000 = h+1
-            let zz = 'answers' + String(index000);
-            if(document.getElementById('choice' + String(h) + String(i)).checked === true && q[]){
-                console.log('hi')
-
-            } 
-        }
-        
-            
-    }
-    */
-    /*
-    for(let i = 0; i < 4; i++){
-        
-    }
-    */
-    /*
-    if(choice01.checked === true){
-        console.log('hi')
-    }
-    */
-   
-   return score;
+    return score;
 }
 
-/*
-function displayQuiz(quiz){
-    let quizDisplay = document.createElement('div');
-    quizDisplay.setAttribute('id', 'quizDisplay');
-    //for(let i = 0; i < )
 
-    let q1 = document.createTextNode(quiz[0].question1);
-    
-    for(let i = 0; i < 4; i++){
-        let choice = document.createElement('input');
-        let choiceTxt = document.createElement('label');
-        choice.setAttribute('type', 'radio');
-        choice.setAttribute('id', 'choice' + String(i));
-        choice.setAttribute('name', 'choice')
-        //choiceTxt.textContent = quiz[0].answers1[0][property[0]];
-        choiceTxt.textContent = quiz[0].answers1[i][i];
-        console.log(choice)
-        choiceTxt.setAttribute('for', choice);
-        quizDisplay.appendChild(addPre());
-        quizDisplay.appendChild(choice)
-        quizDisplay.appendChild(choiceTxt)
-    }
-    
-    
-    document.body.appendChild(quizDisplay);
-    //return quiz;
-}
-
-*/
-//saveState();
-/*
-
-function displayQuiz(quiz){
-    let quizDisplay = document.createElement('div');
-    quizDisplay.setAttribute('id', 'quizDisplay');
-    //for(let i = 0; i < )
-    let q1 = document.createTextNode(quiz[0].question1);
-    let choiceA = document.createElement('input');
-    let aText = document.createElement('label');
-    const pre = document.createElement('pre');
-    choiceA.setAttribute('type', 'radio')
-    choiceA.setAttribute('id', 'choiceA')
-    aText.textContent =  quiz[0].answers1[0].a
-
-    aText.setAttribute('for', 'choiceA')
-    quizDisplay.appendChild(q1)
-    quizDisplay.appendChild(pre)
-    quizDisplay.appendChild(choiceA);
-    quizDisplay.appendChild(aText)
-    //quizDisplay.innerText = normalLevelCoding[0].question1;
-
-
-    document.body.appendChild(quizDisplay);
-    //return quiz;
-}
-
-*/
-//displayQuiz(normalLevelCoding);
 
 //When called, this fucnction will allow each button to be clicked.
-function enableButtons(){
+function enableButtons() {
     difficulty.removeAttribute("disabled");
     timeLimit.removeAttribute("disabled");
     startBtn.removeAttribute("disabled");
@@ -264,61 +167,32 @@ bballPic.addEventListener("click", () => {
 startBtn.addEventListener("click", () => {
     initDisplay.style.display = "none";//hide the 'start ment' display
 
-    if(startBtn.classList.contains("codingPic")){
-        if(difficulty.value === "normal"){
+    if (startBtn.classList.contains("codingPic")) {
+        if (difficulty.value === "normal") {
             displayQuiz(normalLevelCoding);
-
-
         }
-        else{
+        else {
             displayQuiz(hardLevelCoding);
         }
 
     }
-    else if(startBtn.classList.contains("mathPic")){
+    else if (startBtn.classList.contains("mathPic")) {
+        if (difficulty.value === "normal") {
+            displayQuiz(normalLevelMath);
+        }
+        else {
+            displayQuiz(hardLevelMath);
+        }
+
 
     }
-    else if(startBtn.classList.contains("bballPic")){
-
+    else if (startBtn.classList.contains("bballPic")) {
+        if (difficulty.value === "normal") {
+            displayQuiz(normalLevelBball);
+        }
+        else {
+            displayQuiz(hardLevelBball);
+        }
     }
 
-    //displayQuiz();
-    
 });
-
-
-/*
-codingPic.addEventListener("click", () => {
-    enableButtons();
-});
-
-
-mathPic.addEventListener("click", () => {
-    enableButtons();
-});
-*/
-
-/*
-function func(){
-    let x = 3;
-    return x;
-}    
-
-mathPic.addEventListener("click", func)
-
-let x = func();
-console.log(x)
-*/
-
-    /*
-    topics.style.display = "none";
-    document.getElementById('header2').style.display = "none";
-    const difficulty = document.createElement("div"); 
-    const normal_diff = document.createElement("input");
-    normal_diff.setAttribute('type', 'button');
-    normal_diff.setAttribute('value', 'Normal');
-    normal_diff.setAttribute("id", "normal");
-    difficulty.appendChild(normal_diff);
-    document.body.appendChild(difficulty);
-    */
-
--- docs/bushey_questions_answers.js
+++ docs/bushey_questions_answers.js
@@ -42,10 +42,182 @@ const normalLevelCoding = [
 
 const hardLevelCoding = [
     {
+        question1: "Question 1: What is the difference between array.filter and array.map?",
+        answers1: [
+                {0: "Filter pops elements off the array and returns that array. Map creates a new array"},
+                {1: "Filter creates a new array by filtering out certain elements of the old array. Map filters out the elements and returns the same array."},
+                {2: "Filter creates a new array by filtering out certain elements of the old array. Map creates a new array based off the old array with certain adjustments."},
+                {3: "Filter filters out an array and returns a string. Map filters out an array and returns a new array."}
+        ],
+        correct: 2
+    },
 
+    {
+        question2: "Question 2: Which statement is true?",
+        answers2: [
+                {0: "The class name and the file name must be the same in Java. In JavaScript, a file can be named whatever as long as it has the .js extension."},
+                {1: "Only classes can be used in Java"},
+                {2: "JS has 'int' type while Java only uses 'number' type"},
+                {3: "JavaScript is mostly used for Android apps"}
+        ],
+        correct: 1
+    },
+
+    {
+        question3: "Question 3: (Github) Which statement is false?",
+        answers3: [
+                {0: "The 'git push' command will save the commit to the local computer."},
+                {1: "The 'git push' command will save the commit to a remote repository"},
+                {2: "The 'git add .' is another command that will change the remote repository"},
+                {3: "Github desktop is required to use Git"},
+        ],
+        correct: 1
     }
 ]
 
+const normalLevelMath = [
+    {
+        question1: "Question 1: What is the correct order of opreations?",
+        answers1: [
+            {0: "DMPEAS"},
+            {1: "PEMDAS"},
+            {2: "EPMDAS"},
+            {3: "PEMDSA"},
+        ],
+        correct: 1
+    },
+
+    {
+        question2: "Question 2: What is the correct distribution of 3(x+1)?",
+        answers2: [
+            {0: "4"},
+            {1: "3"},
+            {2: "3x + 3"},
+            {3: "3x + 1"},
+        ],
+        correct: 2
+    },
+
+    {
+        question3: "Question 3: What does 4(x + 3x), x = 2, evaluate to?",
+        answers3: [
+            {0: "7"},
+            {1: "12"},
+            {2: "24"},
+            {3: "32"},
+        ],
+        correct: 3
+    }
+    
+]
+
+const hardLevelMath = [
+    {
+        question1: "Question 1: What does 4(3x + 3^(x) - 31) evaluate to?",
+        answers1: [
+            {0: "1"},
+            {1: "32"},
+            {2: "64"},
+            {3: "-64"},
+        ],
+        correct: 3
+    },
+
+    {
+        question2: "Question 2: What is the derivative of 3x?",
+        answers2: [
+            {0: "0"},
+            {1: "3x^2"},
+            {2: "3"},
+            {3: "3x"},
+        ],
+        correct: 2
+    },
+
+    {
+        question3: "Question 3: What does 5! evaluate to?",
+        answers3: [
+            {0: "0"},
+            {1: "5"},
+            {2: "120"},
+            {3: "60"},
+        ],
+        correct: 2
+    },
+    
+]
+
+const normalLevelBball = [
+    {
+        question1: "Question 1: How many points is a free throw worth?",
+        answers1: [
+            {0: "1"},
+            {1: "2"},
+            {2: "7"},
+            {3: "3"},
+        ],
+        correct: 0
+    },
+
+    {
+        question2: "Question 2: How many steps can you take once you have picked up your dribble?",
+        answers2: [
+            {0: "3"},
+            {1: "0"},
+            {2: "2"},
+            {3: "1"},
+        ],
+        correct: 2
+    },
+
+    {
+        question3: "Question 3: When does an 'offensive foul' occur?",
+        answers3: [
+            {0: "When the offensive player collides with a defensive player. The defender must be stationary, have both feet flat on the ground, and be facing the offensive player"},
+            {1: "When a defender charges the opponent player who has the ball."},
+            {2: "When the defender grabs the ball after it has been shot by another player and it's trajectory is downward."},
+            {3: "When the offensive player steps over the freethrow line while shooting freethrows."},
+        ],
+        correct: 0
+    },
+    
+]
+
+const hardLevelBball = [
+    {
+        question1: "Question 1: Who currently has the most points in NBA history?",
+        answers1: [
+            {0: "Michael Jordan"},
+            {1: "Wilt Chamberlain"},
+            {2: "Kareem Abdul-Jabbar"},
+            {3: "Larry Bird"},
+        ],
+        correct: 2
+    },
+
+    {
+        question2: "Question 2: What 37 year old NBA star currently ranks 2nd in points all time",
+        answers2: [
+            {0: "Kevin Durant"},
+            {1: "Steph Curry"},
+            {2: "Lebron James"},
+            {3: "Giannis Antetokounmpo"},
+        ],
+        correct: 2
+    },
+
+    {
+        question3: "Question 3: What year was LeBron James drafted into the NBA?",
+        answers3: [
+            {0: "2004"},
+            {1: "2010"},
+            {2: "2003"},
+            {3: "2002"},
+        ],
+        correct: 2
+    },
+    
+]
 
 //console.log(typeof(normalLevelCoding[0].correct))
-export { normalLevelCoding, hardLevelCoding };
+export { normalLevelCoding, hardLevelCoding, normalLevelMath, hardLevelMath, normalLevelBball, hardLevelBball };
